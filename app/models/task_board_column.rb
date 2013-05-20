ebbdb978437b8bd746b21b14db9059ec26fd6df0
GOZU Kenichiro <k.gozu@infocom.co.jp>
2013-05-20 22:42:21 +0900

    列にチケットの数とWIPの制限数を表示(WIPの制限数は0なら非表示)

--- app/models/task_board_column.rb
+++ app/models/task_board_column.rb
@@ -5,6 +5,8 @@ class TaskBoardColumn < ActiveRecord::Base
   validates_presence_of :title, :project
   validates_length_of :title, :maximum => 255
 
+#  @@issue_count
+
   def self.empty_status(status_id)
     columns = TaskBoardColumn \
       .select(:id) \
@@ -16,12 +18,14 @@ class TaskBoardColumn < ActiveRecord::Base
 
   def issues(order_column="project_weight")
   	@column_statuses = Hash.new
+    @@issue_count = 0
   	self.issue_statuses.order(:name).each do |status|
   		@column_statuses[status.id] = Array.new
   		issues = Issue.select("issues.*, tbi.is_archived, tbi.#{order_column} as weight, tbi.issue_id") \
   			.joins('LEFT OUTER JOIN task_board_issues AS tbi ON tbi.issue_id = issues.id') \
   			.where("project_id = ? AND status_id = ? AND (is_archived IS NULL OR is_archived = 0)", self.project_id, status.id) \
   			.order("weight ASC, created_on ASC")
+      @@issue_count += issues.count
   		issues.each do |issue|
         # Create a TaskBoardIssue (i.e. a Card) if one doesn't exist already.
   			unless issue.issue_id
@@ -36,6 +40,10 @@ class TaskBoardColumn < ActiveRecord::Base
   		end
   	end
   	return @column_statuses
+  end
+
+  def issues_count()
+    return @@issue_count
   end
 
 end
